2ebbb9dffaf5e10798c97c08e347880c9f54fe23
Stephen Bau <bauhouse@gmail.com>
2010-04-02 12:21:00 -0700

    Create Forum: Code Source data source

---
+++ workspace/data-sources/data.forum_code_source.php
@@ -0,0 +1,67 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+	
+	Class datasourceforum_code_source extends Datasource{
+		
+		public $dsParamROOTELEMENT = 'forum-code-source';
+		public $dsParamORDER = 'desc';
+		public $dsParamLIMIT = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'system:id';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+		
+		public $dsParamFILTERS = array(
+				'id' => '{$comment-id}',
+		);
+		
+		public $dsParamINCLUDEDELEMENTS = array(
+				'comment: formatted'
+		);
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+		
+		public function about(){
+			return array(
+					 'name' => 'Forum: Code Source',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'website' => 'http://home/sym/sym-forum',
+							'email' => 'bauhouse@gmail.com'),
+					 'version' => '1.0',
+					 'release-date' => '2010-04-02T19:18:58+00:00');	
+		}
+		
+		public function getSource(){
+			return '3';
+		}
+		
+		public function allowEditorToParse(){
+			return true;
+		}
+		
+		public function grab(&$param_pool=NULL){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+				
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}			
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}	
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+			return $result;
+		}
+	}
+
