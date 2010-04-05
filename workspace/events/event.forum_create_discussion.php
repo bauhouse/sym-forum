2182a1dfa575ff211800cecbaae5f552d3be7380
Stephen Bau <bauhouse@gmail.com>
2010-04-05 12:22:41 -0700

    Make change required to Forum: Create Discussion event customized for EventEx and Members extension compatibility - http://symphony-cms.com/discuss/thread/22126/4/#position-71

--- workspace/events/event.forum_create_discussion.php
+++ workspace/events/event.forum_create_discussion.php
@@ -30,52 +30,6 @@
 			return array("discussions", "comments");
 		}
 
-		public static function documentation(){
-			return '
-        <h3>Success and Failure XML Examples</h3>
-        <p>When saved successfully, the following XML will be returned:</p>
-        <pre class="XML"><code>&lt;forum-create-discussion result="success" type="create | edit">
-  &lt;message>Entry [created | edited] successfully.&lt;/message>
-&lt;/forum-create-discussion></code></pre>
-        <p>When an error occurs during saving, due to either missing or invalid fields, the following XML will be returned:</p>
-        <pre class="XML"><code>&lt;forum-create-discussion result="error">
-  &lt;message>Entry encountered errors when saving.&lt;/message>
-  &lt;field-name type="invalid | missing" />
-  ...
-&lt;/forum-create-discussion></code></pre>
-        <h3>Example Front-end Form Markup</h3>
-        <p>This is an example of the form markup you can use on your frontend:</p>
-        <pre class="XML"><code>&lt;form method="post" action="" enctype="multipart/form-data">
-  &lt;input name="MAX_FILE_SIZE" type="hidden" value="5242880" />
-  &lt;label>Topic
-    &lt;input name="fields[topic]" type="text" />
-  &lt;/label>
-  &lt;label>Created By
-    &lt;input name="fields[created-by]" type="text" />
-  &lt;/label>
-  &lt;label>Date
-    &lt;input name="fields[date]" type="text" />
-  &lt;/label>
-  &lt;label>Last Post
-    &lt;input name="fields[last-post]" type="text" />
-  &lt;/label>
-  &lt;label>Last Active
-    &lt;input name="fields[last-active]" type="text" />
-  &lt;/label>
-  &lt;label>Pinned
-    &lt;input name="fields[pinned]" type="checkbox" />
-  &lt;/label>
-  &lt;label>Closed
-    &lt;input name="fields[closed]" type="checkbox" />
-  &lt;/label>
-  &lt;input name="action[forum-create-discussion]" type="submit" value="Submit" />
-&lt;/form></code></pre>
-        <p>To edit an existing entry, include the entry ID value of the entry in the form. This is best as a hidden field like so:</p>
-        <pre class="XML"><code>&lt;input name="id" type="hidden" value="23" /></code></pre>
-        <p>To redirect to a different location upon a successful save, include the redirect location in the form. This is best as a hidden field like so, where the value is the URL to redirect to:</p>
-        <pre class="XML"><code>&lt;input name="redirect" type="hidden" value="http://home/sym/test/success/" /></code></pre>';
-		}
-
 		protected function __trigger(){
 			// this returns a Symphony XMlElement object
 			$result = $this->updateNamedSections(self::getSource());
@@ -85,3 +39,4 @@
 
 	}
 
+class forum_create_discussion extends GenericSectionUpdate { }
